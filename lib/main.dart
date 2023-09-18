f4d792bb890ec7ac66d6e4a838395af3e1440689
rhit-trannm <trannm@rose-hulman.edu>
2023-09-18 17:49:01 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,78 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 void main() {
-  runApp(TicTacToeApp());
+  runApp(MyApp());
 }
 
-class TicTacToeApp extends StatelessWidget {
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'TicTacToe App',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
-        ),
-        home: MyHomePage(),
-      );
+      title: 'Flutter Side Menu Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-    List<String> board = List.filled(9, ''); // Represents the game board.
-    bool isXNext = true;
-    Widget _buildTile(int index) {
-      return GestureDetector(
-        onTap: () {
-          _onTileTapped(index);
-        },
-        child: Container(
-          decoration: BoxDecoration(
-            border: Border.all(),
-          ),
-          child: Center(
-            child: Text(
-              board[index],
-              style: TextStyle(fontSize: 40),
-            ),
-          ),
-      ),
-      );
-    }
-  void _onTileTapped(int index) {
-    if (board[index] == '') {
-      setState(() {
-        board[index] = isXNext ? 'X' : 'O';
-        isXNext = !isXNext;
-      });
-    }
-  }
-  bool checkForWin() {
-    // Check rows
-    for (int i = 0; i < 9; i += 3) {
-      if (board[i] != '' &&
-          board[i] == board[i + 1] &&
-          board[i] == board[i + 2]) {
-        return true;
-      }
-    }
-    // Check columns
-    for (int i = 0; i < 3; i++) {
-      if (board[i] != '' &&
-          board[i] == board[i + 3] &&
-          board[i] == board[i + 6]) {
-        return true;
-      }
-    }
-    // Check diagonals
-    if (board[0] != '' && board[0] == board[4] && board[0] == board[8]) {
-      return true;
-    }
-    if (board[2] != '' && board[2] == board[4] && board[2] == board[6]) {
-      return true;
-    }
-    return false;
-  }
-
   @override
-  Widget build(BuildContext context) {  
+  Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Main Page'),
+      ),
+      drawer: SideMenu(), // Side column
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('TicTacToe'),
-            SizedBox(height: 20),
-            GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
+        child: Text('Content of the Main Page'),
+      ),
+    );
+  }
+}
+
+class SideMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Text(
+              'Side Menu',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
               ),
-              itemBuilder: (context, index) {
-                return _buildTile(index);
-              },
-              itemCount: 9,
-              shrinkWrap: true,
             ),
-            SizedBox(height: 20),
-            Text(
-              isXNext ? 'Next Player: X' : 'Next Player: O',
-              style: TextStyle(fontSize: 20),
-            ),
-          ],
-        ),
+          ),
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text('Home'),
+            onTap: () {
+              // Handle navigation to the home page.
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Settings'),
+            onTap: () {
+              // Handle navigation to the settings page.
+              Navigator.pop(context);
+            },
+          ),
+        ],
       ),
     );
   }
