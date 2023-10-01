5d7340f5a652e0055e3f768d2ff12ecc4c30971d
rhit-trannm <trannm@rose-hulman.edu>
2023-10-01 17:30:57 -0400

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,78 +1,83 @@
 import 'package:flutter/material.dart';
+class Line {
+  final List<Offset> points;
+  final Color color;
+  final double strokeWidth;
 
-void main() {
-  runApp(MyApp());
+  Line({required this.points, this.color = Colors.black, this.strokeWidth = 5.0});
 }
+
+class DrawingPainter extends CustomPainter {
+  final List<Line> lines;
+
+  DrawingPainter({required this.lines});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    for (var line in lines) {
+      if (line.points.isEmpty) continue;
+      final paint = Paint()
+        ..color = line.color
+        ..strokeWidth = line.strokeWidth
+        ..strokeCap = StrokeCap.round;
+      for (int i = 0; i < line.points.length - 1; i++) {
+        if (line.points[i] != null && line.points[i + 1] != null)
+          canvas.drawLine(line.points[i], line.points[i + 1], paint);
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class DrawingCanvas extends StatefulWidget {
+  @override
+  _DrawingCanvasState createState() => _DrawingCanvasState();
+}
+
+class _DrawingCanvasState extends State<DrawingCanvas> {
+  List<Line> lines = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onPanUpdate: (details) {
+        setState(() {
+          final renderBox = context.findRenderObject() as RenderBox;
+          final localPosition = renderBox.globalToLocal(details.globalPosition);
+          final currentLine = (lines.isNotEmpty && lines.last.points.isNotEmpty)
+              ? lines.last
+              : null;
+
+          if (currentLine == null) {
+            lines.add(Line(points: [localPosition]));
+          } else {
+            currentLine.points.add(localPosition);
+          }
+        });
+      },
+      onPanEnd: (details) {
+        lines.add(Line(points: []));
+      },
+      child: CustomPaint(
+        painter: DrawingPainter(lines: lines),
+        child: Container(),
+      ),
+    );
+  }
+}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Side Menu Example',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Main Page'),
-      ),
-      drawer: SideMenu(), // Side column
-      body: Center(
-        child: Text('Content of the Main Page'),
-      ),
-    );
-  }
-}
-
-class SideMenu extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: <Widget>[
-          DrawerHeader(
-            decoration: BoxDecoration(
-              color: Colors.blue,
-            ),
-            child: Text(
-              'Side Menu',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 24,
-              ),
-            ),
-          ),
-          ListTile(
-            leading: Icon(Icons.home),
-            title: Text('Home'),
-            onTap: () {
-              // Handle navigation to the home page.
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.settings),
-            title: Text('Settings'),
-            onTap: () {
-              // Handle navigation to the settings page.
-              Navigator.pop(context);
-            },
-          ),
-        ],
+      home: Scaffold(
+        appBar: AppBar(title: Text('Draw on Canvas')),
+        body: DrawingCanvas(),
       ),
     );
   }
